f376128297f8011518bd0e682155596953ce0a0a
Bill Hsu <bl33234679@gmail.com>
2020-04-30 01:55:09 +0000

    Update kernel files to v5

--- kernel_files/kernel/get_timestamp.c
+++
@@ -1,7 +0,0 @@
-#include <linux/linkage.h>
-#include <linux/kernel.h>
-#include <linux/timer.h>
-
-asmlinkage void sys_get_timestamp(struct timespec *ts) {
-  getnstimeofday(ts);
-}
--- kernel_files/kernel/print_timestamp.c
+++ kernel_files/kernel/print_timestamp.c
@@ -1,7 +1,9 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
 #include <linux/timer.h>
+#include <linux/syscalls.h>
 
-asmlinkage void sys_print_timestamp(int pid, long s1, long s2, long e1, long e2) {
+SYSCALL_DEFINE5(sys_print_timestamp, int, pid, long, s1, long, s2, long, e1, long, e2) {
   printk(KERN_INFO "[Project1] %d %ld.%09ld %ld.%09ld\n", pid, s1, s2, e1, e2);
+  return 0;
 }
